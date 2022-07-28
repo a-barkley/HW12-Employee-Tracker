3767f0f53c1d214936ee198c6d197566f144c25a
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-07-28 11:27:30 -0500

    add initial questions

--- server.js
+++ server.js
@@ -22,6 +22,61 @@ select last_name as 'Last Name',first_name as 'First Name' from employee;
 
 select e.last_name,e.first_name,r.title from employee e inner join role r on e.role_id = r.id order by e.last_name;
 
+
+function initial() {
+  inquirer
+    .prompt
+    (
+      {
+        name: 'mainQuestions',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: 
+          [
+            'View Departments',
+            'View Roles',
+            'View Employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update employee role'
+          ]
+      }
+    )
+    .then((answer) => {
+      switch (answer.mainQuestions) {
+        case 'View Departments':
+          viewDepartment();
+          break;
+        
+        case 'View Roles':
+          viewRoles();
+          break;
+
+        case 'View Employees':
+          viewEmployees();
+          break;
+
+        case 'Add a department':
+          addDepartment();
+          break;
+
+        case 'Add a role':
+          addRole();
+          break;
+
+        case 'Add an employee':
+          addEmployee();
+          break;
+
+        case 'Update employee role':
+          updateRole();
+          break;
+      }
+    })
+}
+
+
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
