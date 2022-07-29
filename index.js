e7f23f82b8e10065e26de47716fc719d07b6b08e
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-07-28 22:36:52 -0500

    final commit

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const db = mysql.createConnection(
 );
 
 
-// select e.last_name,e.first_name,r.title from employee e inner join role r on e.role_id = r.id order by e.last_name;
-
 function viewDepartments() {
   return new Promise(resolve => {
     db.query("select id, name as 'Department' from department", (err, result) => {
@@ -29,10 +27,9 @@ function viewDepartments() {
   })
 }
 
-
 function viewRoles() {
   return new Promise(resolve => { 
-    db.query("select id, title as 'Position', salary as 'Salary' from role", (err, result) => {
+    db.query("select r.title,r.id,d.name,r.salary from role r inner join department d on r.department_id = d.id order by r.id", (err, result) => {
       if (err) {
         console.log(err)
       } else {
@@ -42,7 +39,6 @@ function viewRoles() {
     })
   })  
 }
-
 
 function viewEmployees() {
   return new Promise(resolve => {
@@ -72,6 +68,38 @@ function addDepartment() {
             console.log(err)
           } else {
             viewDepartments();
+          }
+        })
+        resolve('resolve')
+      })
+  })
+}
+
+// function broken, skipping all but first question which makes it incompatible with the db.query statement
+function addRole() {
+  return new Promise(resolve => {
+    inquirer  
+      .prompt(
+        {
+          name: 'newRole',
+          message: 'What is the name of the role you would like to add?'
+        },
+        {
+          name: 'newSalary',
+          message: 'What is the salary of the new role?'
+        },
+        {
+          name: 'roleDept',
+          message: 'What department does the role belong to?'
+        }
+      )
+      .then((answer) => {
+        console.log('test')
+        db.query(`insert into role (title,salary,department_id) values (?)`, [[answer.newDepartment,answer.newSalary,answer.roleDept]],(err, result) => {
+          if (err) {
+            console.log(err)
+          } else {
+            viewRoles();
           }
         })
         resolve('resolve')
@@ -132,9 +160,6 @@ function initial() {
       initial();
     })
 }
-
-
-
 
 
 initial();
